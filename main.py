9e861efc5842f513d5900a2b5ea576c42b9de946
ivanturner3 <ivanturneriii@gmail.com>
2022-09-22 18:38:59 -0400

    Added translation and exports to docx file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from gettext import find
 from MainConfig import *
 from jira import JIRA
+from deep_translator import GoogleTranslator
+from docx import Document
 
 jira = JIRA(server=jiraServer, basic_auth=(jiraUser, jiraPass))
 
@@ -16,7 +19,6 @@ while foundIssue == False:
         errorMessage = 'Issue does not exist, please try again.'
         print(errorMessage)
     else:
-        print(f'Issue found: {issue}')
         foundIssue = True
 
 class JiraTicket:
@@ -26,16 +28,42 @@ class JiraTicket:
         self.description = jira.issue(self.issueID).fields.description
         self.comments = jira.comments(self.issueID)
 
+ticket = JiraTicket()
 
-print(JiraTicket().summary)
-print(JiraTicket().description)
-for comm in JiraTicket().comments:
+translatedSummary = GoogleTranslator(source='auto', target='en').translate(ticket.summary)
+translatedDescription = GoogleTranslator(source='auto', target='en').translate(ticket.description)
+
+doc = Document()
+#run = doc.add_paragraph()
+doc.add_heading(f"Jira Ticket ID: {ticket.issueID}", 0)
+doc.add_heading("Original Text:", 1)
+doc.add_paragraph(f"Title: {ticket.summary}")
+doc.add_paragraph(f"Description: {ticket.description}")
+for comm in ticket.comments:
     if comm.jsdPublic == True:
-        public = "Replied to customer"
+        public = "Reply to customer"
     if comm.jsdPublic == False:
         public = "Internal comment"
-    print(f'\nDate: {comm.created}')
-    print(f'Internal/Customer facing: {public}')
-    print(f'Author: {comm.author.displayName}')
-    print(f'Comment Body: {comm.body}')
+    originalComment = str(comm.body).replace("\!", "!").strip()
+    # findAccountID = originalComment.find("[~accountid:")
+    # if findAccountID != -1:
+    #     accountID = 
+    para = doc.add_paragraph()
+    para.add_run(f"{public} by: {comm.author.displayName}").bold = True
+    para.add_run(f"\nDate Posted: {comm.created}")
+    doc.add_paragraph(f"{originalComment}")
     
+doc.add_heading("Translated Text:", 1)
+doc.add_paragraph(f"Title: {translatedSummary}")
+doc.add_paragraph(f"Description: {translatedDescription}")
+for comm in ticket.comments:
+    if comm.jsdPublic == True:
+        public = "Reply to customer"
+    if comm.jsdPublic == False:
+        public = "Internal comment"
+    translatedComment = str(comm.body).replace("\!", "!").strip()
+    para = doc.add_paragraph()
+    para.add_run(f"{public} by: {comm.author.displayName}").bold = True
+    para.add_run(f"\nDate Posted: {comm.created}")
+    doc.add_paragraph(f"{GoogleTranslator(source='auto', target='en').translate(translatedComment)}")
+doc.save(f"translations\\{ticket.issueID}.docx")
